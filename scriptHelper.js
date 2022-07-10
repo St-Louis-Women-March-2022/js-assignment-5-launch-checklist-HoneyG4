5a3584200ecd1c00d770b3122486013945aad717
Gabrielle <gabrielle.renee1004@gmail.com>
2022-07-09 22:38:54 -0500

    first commit

--- scriptHelper.js
+++ scriptHelper.js
@@ -2,25 +2,100 @@
 require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
-   /*
-                <h2>Mission Destination</h2>
-                <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
-                    <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
-                </ol>
-                <img src="">
-   */
-}
+    const missionTarget = document.getElementById("missionTarget");
 
+    missionTarget.innerHTML += `
+        <div>
+            <ol>        
+                <li>Name: ${name}</li>
+                <li>Diameter: ${diameter}</li>
+                <li>Star: ${star}</li>
+                <li>Distance: ${distance}</li>
+                <li>Moons: ${moons}</li>
+            </ol>
+            <img src=${imageUrl}></img>
+        </div>`
+        ;
+    }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+    
 function validateInput(testInput) {
+    if (testInput ==="") {
+       alert("Empty")
+    }
+    if (isNaN(value)){
+        alert("Not A Number")
+    }
+    else if{
+       alert("Is a Number")
+    }
+    else {
+
+
+    }
+    }
+    
+ 
+
    
-}
+
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+    let launchStatus = document.getElementById('launchStatus')
+    let pilotStatus = document.getElementById("pilotStatus")
+    let copilotStatus = document.getElementById("copilotStatus")
+    let fuelLevel= document.getElementById('fuelStatus')
+    let  cargoLevel = document.getElementById('cargoStatus')
+    const list = document.querySelector('#faultyItems')
+    launchStatus.innerHTML += `
+    <div>
+        <ol>
+            `
+        pilotStatus.innerHTML = `Pilot ${pilot} Ready`
+        copilotStatus.innerHTML = `CoPilot ${copilot} Ready`;
+
+        if (fuelLevel >= 10000) {
+            list.style.visibility = 'visible';
+                launchStatus.innerHTML = `Shuttle is ready for launch`;
+                    launchStatus.style.color = `green`;
+                        fuelStatus.innerHTML = `Fuel level high enough for launch`;
+
+        }
+        if (cargoMass <= 10000) {
+            list.style.visibility = 'visible';
+                launchStatus.innerHTML = `Shuttle is ready for launch`;
+                    launchStatus.style.color = `green`;
+                        cargoLevel.innerHTML = `Cargo mass low enough for launch`;
+
+        }    
+        if (fuelLevel <= 10000){
+            list.style.visibility = 'visible';
+                launchStatus.innerHTML = `Shuttle not ready for launch`;
+                    launchStatus.style.color = `red`;
+                        fuelStatus.innerHTML = `Fuel level too low for launch`;
+        }
+        if (cargoMass >= 10000) {
+            list.style.visibility = 'visible';
+                launchStatus.innerHTML = `Shuttle not ready for launch`;
+                    launchStatus.style.color = `red`;
+                    cargoLevel.innerHTML = `Cargo mass over capacity for launch`;
+        }                                                     
+                `
+            </ol>
+        </div>
+        `; 
+
+
    
 }
 
